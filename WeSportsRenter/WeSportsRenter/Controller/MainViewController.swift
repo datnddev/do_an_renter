df97904d6864cf1281385399d0c2d6325cfbb3fe
Dat Nguyen <datnd.dev@gmail.com>
2021-10-19 21:07:02 +0700

    initial project

---
+++ WeSportsRenter/WeSportsRenter/Controller/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WeSportsRenter
+//
+//  Created by datNguyem on 19/10/2021.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
